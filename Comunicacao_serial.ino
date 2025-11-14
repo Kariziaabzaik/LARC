9eaa215e235a8f6a4b94765f8b2c8dc828696fce
Kariziaabzaik <gercina.karizia@ufpe.br>
2025-11-14 11:22:09 -0300

    Update Comunicacao_serial.ino

--- Comunicacao_serial.ino
+++ Comunicacao_serial.ino
@@ -1,21 +1,27 @@
-char Dados;  //Definição do tipo de variável
+char Dados; // Definição do tipo de variável
 
 void setup() {
-  Serial.begin(115200);  //Habilitar comunicação serial ESP32
-  pinMode(19, OUTPUT);   //Habilitar pino 19 como saída
+  Serial.begin(115200);   // Habilitar comunicação serial ESP32
+  pinMode(23, OUTPUT);    // Habilitar pino 23 como saída
+  
+  Serial.println("ESP32 pronto. Envie 'A' para ligar ou 'a' para desligar o pino 23."); 
 }
 
 void loop() {
-  Dados = Serial.read();  //Leitura da porta serial
+  // Verifica se há dados disponíveis para ler
+  if (Serial.available() > 0) {
+    
+    // Lê o dado
+    Dados = Serial.read(); 
 
-  if (Dados == 'A')
-    {
-    digitalWrite(19, HIGH);
-    Serial.println("Pino 19 ativado");
+    // Compara o dado lido
+    if (Dados == 'A') {
+      digitalWrite(23, HIGH);
+      Serial.println("Pino 23 ativado");
+    } 
+    else if (Dados == 'a') {
+      digitalWrite(23, LOW);
+      Serial.println("Pino 23 desativado");
     }
-  if (Dados == 'a')
-    {
-    digitalWrite(19, LOW);
-    Serial.println("Pino 19 desativado");
-    }
+  }
 }
