aae3d268a9e458d31de0f306788913da2282791b
Kariziaabzaik <gercina.karizia@ufpe.br>
2025-11-14 22:08:07 -0300

    Enhance servo control with comments and PWM setup
    Added comments and configured PWM frequency for servo.

--- ESP32/Controle_Servo.ino
+++ ESP32/Controle_Servo.ino
@@ -1,3 +1,8 @@
+/*
+ * Controle de Servo Motor com ESP32
+ * Este código faz um servo motor varrer continuamente
+ * de 0 a 180 graus e depois retornar de 180 a 0 graus.
+ */
 #include <ESP32Servo.h>
 
 static const int servoPin = 23;
@@ -12,16 +17,19 @@ int angMax = 180;
 
 void setup() {
     Serial.begin(115200);
+    servo.setPeriodHertz(50); // Configura a frequência do sinal PWM para 50Hz
     servo.attach(servoPin);
 }
 
 void loop() {
+    // Inicia um loop 'for' que vai de 'angMin' (0) até 'angMax' (180)
     for(int ang = 0; ang <= angMax; ang +=angStep) {
         servo.write(ang);
         Serial.println(ang);
         delay(20);
     }
-
+    
+    // --- Segundo movimento: de 180 para 0 graus ---
     for(int ang = 180; ang >= angMin; ang -=angStep) {
         servo.write(ang);
         Serial.println(ang);
