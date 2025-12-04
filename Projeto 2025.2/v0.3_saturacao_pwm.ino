69344a054b0b62ba2c2a562c4ef7822c98ba516a
j000tape <joaopaulolbe06@gmail.com>
2025-12-04 11:57:41 -0300

    Update v0.3_saturacao_pwm.ino

--- Projeto 2025.2/v0.3_saturacao_pwm.ino
+++ Projeto 2025.2/v0.3_saturacao_pwm.ino
@@ -1,1 +1,21 @@
-#include 
+
+const int freq = 50
+const int ledchannel = 0
+const int resolucao = 16
+const int pino = 13
+
+void setup(){
+  ledcSetup(ledchannel, freq, resolucao);
+  ledcAttachPin(pino, ledchannel);
+}
+
+void loop(){
+  ledcWrite(ledchannel, 4915);
+  delay(1000);
+
+  ledcWrite(ledchannel, 1638);
+  delay(1000);
+
+  ledcWrite(ledchannel, 8192);
+  delay(1000);
+}
